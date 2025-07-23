cd18a762ad00d996df9b721de92d9cd758cdeea2
Niek Peters <n.s.peters05@gmail.com>
2025-07-23 15:22:45 +0200

    Readied for deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,19 @@
 	import { code } from '$lib/code.svelte';
 	import { error } from '$lib/error.svelte';
 	import { name } from '$lib/name.svelte';
+	import { dev } from '$app/environment';
 
 	let { children } = $props();
 
 	let askCode = $state(false);
 	let askName = $state(false);
+	let joever = $state(false);
 
 	const { promise: ws, resolve: res } = Promise.withResolvers<WebSocket>();
 
 	onMount(() => {
-		const ws = new WebSocket('http://localhost:3000');
+		const ws = new WebSocket(dev ? 'http://localhost:3000' : 'https://api.bier.instituutegel.nl');
+		ws.onerror = () => (joever = true);
 		ws.onopen = () => {
 			res(ws);
 			ws.send(JSON.stringify({ code: code.state, name: name.state }));
@@ -42,59 +45,63 @@
 </script>
 
 <div class="relative flex min-h-screen w-screen flex-col">
-	<header>
-		<p>
-			{#if name.state !== ''}
-				Welkom {name.state}!
-			{:else}
-				Goedendag
-			{/if}
-		</p>
-	</header>
+	{#if joever}
+		<p>It's SO Joever</p>
+	{:else}
+		<header>
+			<p>
+				{#if name.state !== ''}
+					Welkom {name.state}!
+				{:else}
+					Goedendag
+				{/if}
+			</p>
+		</header>
 
-	{@render children()}
+		{@render children()}
 
-	<div class="absolute h-screen w-screen">
-		<div class="relative flex h-screen w-screen flex-col items-center justify-center">
-			{#if askCode}
-				<form
-					onsubmit={async (e) => {
-						e.preventDefault();
-						const form = new FormData(e.currentTarget);
-						const code_ = form.get('code');
-						if (code_ === null) return error.set('Nuh uh');
-						const code__ = code_.toString();
-						if (code_ === '') return error.set('Nuh uh');
-						code.setCode(code__);
-						(await ws).send(JSON.stringify({ code: code.state, name: name.state }));
-					}}
-					class="absolute flex flex-col bg-lime-200"
-				>
-					<p>Wat is de code?</p>
-					<input type="text" name="code" placeholder="De code?" />
-				</form>
-			{:else if askName}
-				<form
-					onsubmit={async (e) => {
-						e.preventDefault();
-						const form = new FormData(e.currentTarget);
-						const name_ = form.get('name');
-						if (name_ === null) return error.set('Nuh uh');
-						const name__ = name_.toString();
-						if (name_ === '') return error.set('Nuh uh');
-						name.setName(name__);
-						(await ws).send(JSON.stringify({ code: code.state, name: name.state }));
-					}}
-					class="absolute flex flex-col bg-lime-200"
-				>
-					<p>En uw naam?</p>
-					<input type="text" name="name" placeholder="Joep v.d. TV show" />
-				</form>
-			{/if}
+		<div class="absolute h-screen w-screen">
+			<div class="relative flex h-screen w-screen flex-col items-center justify-center">
+				{#if askCode}
+					<form
+						onsubmit={async (e) => {
+							e.preventDefault();
+							const form = new FormData(e.currentTarget);
+							const code_ = form.get('code');
+							if (code_ === null) return error.set('Nuh uh');
+							const code__ = code_.toString();
+							if (code_ === '') return error.set('Nuh uh');
+							code.setCode(code__);
+							(await ws).send(JSON.stringify({ code: code.state, name: name.state }));
+						}}
+						class="absolute flex flex-col bg-lime-200"
+					>
+						<p>Wat is de code?</p>
+						<input type="text" name="code" placeholder="De code?" />
+					</form>
+				{:else if askName}
+					<form
+						onsubmit={async (e) => {
+							e.preventDefault();
+							const form = new FormData(e.currentTarget);
+							const name_ = form.get('name');
+							if (name_ === null) return error.set('Nuh uh');
+							const name__ = name_.toString();
+							if (name_ === '') return error.set('Nuh uh');
+							name.setName(name__);
+							(await ws).send(JSON.stringify({ code: code.state, name: name.state }));
+						}}
+						class="absolute flex flex-col bg-lime-200"
+					>
+						<p>En uw naam?</p>
+						<input type="text" name="name" placeholder="Joep v.d. TV show" />
+					</form>
+				{/if}
 
-			{#if error.state !== ''}
-				<div class="absolute bottom-4 bg-red-200">{error.state}</div>
-			{/if}
+				{#if error.state !== ''}
+					<div class="absolute bottom-4 bg-red-200">{error.state}</div>
+				{/if}
+			</div>
 		</div>
-	</div>
+	{/if}
 </div>
